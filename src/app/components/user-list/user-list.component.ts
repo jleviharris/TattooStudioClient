626559d5cced3f3d51f23433ae3a85f720e3f989
Jonathan Harris <jleviharris@aol.com>
2025-02-08 22:21:42 -0500

    Angular projects basics set up, Home page is displaying list of users

---
+++ src/app/components/user-list/user-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.scss'
+})
+export class UserListComponent implements OnInit {
+  users: any[] = [];
+
+  private apiService = inject(ApiService);
+
+  ngOnInit(): void {
+    this.apiService.getUsers().subscribe(
+      (data) => {
+        this.users = data;
+      },
+      (error) => {
+        console.error('Error fetching users', error);
+      }
+    );
+  }
+}
